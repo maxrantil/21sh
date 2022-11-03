d4cd7ec54dc118f924c22bd67d0591387fc2c27b
mqx <litnar@pm.me>
2022-11-03 11:39:21 +0200

    redirect to simple file works now(home session)

--- srcs/lexer/lexer.c
+++ srcs/lexer/lexer.c
@@ -53,7 +53,6 @@ static char	*ft_strsub(char const *s, unsigned int start, size_t len)
 	return (ss);
 }
 
-
 int	peek(char **ptr_to_str, char *toks)
 {
 	char *p;
@@ -92,7 +91,7 @@ int	get_token(char **ptr_to_str, char **token, char **end_q)
 {
 	char	*p;
 	int		ret;
-	
+
 	p = *ptr_to_str;
 	p = skip_whitespaces(p);
 	if (*p == '\0')
@@ -112,26 +111,43 @@ typedef struct s_node
 {
 	int				type;
 	char			*arg[100];
+	struct s_node	*command;
 	struct s_node	*left;
 	struct s_node	*right;
 }					t_node;
 
-typedef struct s_root
+t_node	*create_node(int type, t_node *sub_cmd, t_node *left, t_node *right)
 {
-	t_node	*root;
-}				t_root;
-
-t_node	*create_node(int type, t_node *left, t_node *right)
-{
-	t_node	*node;
+	t_node *node;
 
 	node = (t_node *)malloc(sizeof(t_node));
-	if (node != NULL)
+	node->type = type;
+	node->command = sub_cmd;
+	node->left = left;
+	node->right = right;
+	return (node);
+}
+
+t_node	*parse_redirection(t_node *node, char **str)
+{
+	char	*token;
+	char	*end_q;
+	int		type;
+
+	while (peek(str, "<>"))
 	{
-		memset(node, 0, sizeof(t_node));
-		node->type = type;
-		node->left = left;
-		node->right = right;
+		type = get_token(str, &token, 0);
+		if (get_token(str, &token, &end_q) != 'a')
+		{
+			printf("syntax error near unexpected token `redirect'\n");
+			exit(1);
+		}
+		//if here: token == file name
+		if (type == '>')
+		{
+			node = create_node(REDIR, node, NULL, NULL);
+			node->arg[0] = ft_strsub(token, 0, end_q - token);
+		}
 	}
 	return (node);
 }
@@ -142,20 +158,21 @@ t_node *parse_exec(char **str)
 	char *end_q;
 	size_t	t = 0;
 	t_node *node;
+	// t_node *redir;
 
-	node = create_node(EXEC, NULL, NULL);
-	while (**str && !peek(str, "<|&>#;"))
+	// redir = NULL;
+	node = create_node(EXEC, NULL, NULL, NULL);
+	node = parse_redirection(node, str);
+	while (**str && !peek(str, "|&;"))
 	{
 		int ret = get_token(str, &token, &end_q);
 		if (ret == 'a')
-		{
-			node->arg[t] = ft_strsub(token, 0, end_q - token);
-			t++;
-		}
-		/* else if (ret == '>')
-			node = create_node(REDIR, NULL, node); */
+			node->arg[t++] = ft_strsub(token, 0, end_q - token);
+		node = parse_redirection(node, str);
 	}
 	node->arg[t] = NULL;
+	// if (redir)
+		// return (redir);
 	return (node);
 }
 
@@ -169,16 +186,8 @@ t_node	*parse_pipe(char **ptr_to_str)
 	if (peek(ptr_to_str, "|"))
 	{
 		get_token(ptr_to_str, &token, &end_q);
-		node = create_node(PIPE, node, parse_pipe(ptr_to_str));
+		node = create_node(PIPE, NULL, node, parse_pipe(ptr_to_str));
 	}
-	return (node);
-}
-
-t_node	*parse_line(char **ptr_to_str)
-{
-	t_node	*node;
-
-	node = parse_pipe(ptr_to_str);
 	return (node);
 }
 
@@ -235,16 +244,10 @@ int	open_check(char *filename, int mode)
 	int	file_fd;
 
 	file_fd = -1;
-	if (mode == 1)	// overwrite/create >
-	{
-		// file_fd = open(filename, O_RDWR | O_CREAT | O_TRUNC | O_CLOEXEC, 0644);
-		file_fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0644);
-	}
-	else if (mode == 2)	// append/create >>
-	{
-		// file_fd = open(filename, O_RDWR | O_CREAT | O_APPEND | O_CLOEXEC, 0644);
+	if (mode == 1)
+		file_fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	else if (mode == 2)
 		file_fd = open(filename, O_RDWR | O_CREAT | O_APPEND, 0644);
-	}
 	if (file_fd == -1)
 	{
 		write(2, "error on open_mode\n", 19);
@@ -271,19 +274,18 @@ void	redirection_file(t_node *node)
 	int file_fd;
 	int	dup2_fd;
 
-		// check if (!node->right), SEGFAULT?
-	file_fd = open_check(node->left->arg[0], 1);	//	1 == > , 2 == >>
+	close(1);
+	file_fd = open_check(node->arg[0], 1);	//	1 == > , 2 == >>
+	// dup2_fd = dup2_check(file_fd);
+	// if (fork_check() == 0)
+	// {
+		// exec_command(node->right);
+	// }
+	exec_command(node->command);
 
-	// printf("STR: %s\n", node->right->arg[0]);
-	write(2, "STR: ", 5);
-	dup2_fd = dup2_check(file_fd);
-	if (fork_check() == 0)
-	{
-		exec_command(node->right);
-	}
-	wait(0);
-	close(file_fd);
-	exit(7);
+	// wait(0);
+	// close(file_fd);
+	// exit(7);
 }
 
 void	exec_command(t_node *node)
@@ -301,8 +303,8 @@ void	exec_command(t_node *node)
 	}
 	else if (node->type == PIPE)
 		exec_pipe_node(node);
-	/* else if (node->type == REDIR)
-		redirection_file(node); */
+	else if (node->type == REDIR)
+		redirection_file(node);
 	exit(0);
 }
 
@@ -319,10 +321,15 @@ void	rec_print_tree(t_node *root, int lvl)
 		printf("%s\n", root->arg[0]);
 	else if (root->type == PIPE)
 		printf("|");
+	else if (root->type == REDIR)
+	{
+		printf("%s", root->command->arg[0]);
+		printf(" > %s\n", root->arg[0]);
+	}
 	rec_print_tree(root->left, lvl);
 }
 
-void print_tree(t_node *root) 
+void print_tree(t_node *root)
 {
 	rec_print_tree(root, 0);
 }
@@ -330,11 +337,12 @@ void print_tree(t_node *root)
 int main()
 {
 	// char	*str = "ps aux | grep mrantil | grep -v grep | grep 8 | wc -l";
-	char	*str = "ls | grep a.out | wc -l";
+	char	*str = "ls -la | grep a.out > test.txt";
 	t_node	*root;
 
-	root = parse_line(&str);
+	root = parse_pipe(&str);
 	print_tree(root);
+	write(1, "\n", 1);
 	exec_command(root);
 	exit(0);
 }
