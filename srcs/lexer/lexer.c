75b6ebdf8d8a7e35fd87f6bd7e619cdbdd08782b
Max <rantil@pm.me>
2022-11-04 11:12:52 +0200

    lexer working fine now i guess

--- srcs/lexer/lexer.c
+++ srcs/lexer/lexer.c
@@ -364,6 +364,10 @@ void	rec_print_tree(t_node *root, int lvl)
 		else
 			printf(" %s > %s\n", root->command->arg[0], root->arg[0]);
 	}
+	else if (root->type == AMP)
+		printf("&");
+	else if (root->type == SEMI)
+		printf(";");
 	rec_print_tree(root->left, lvl);
 }
 
@@ -376,10 +380,12 @@ int main()
 {
 	// char	*str = "ps aux | grep mrantil | grep -v grep | grep 8 | wc -l";
 	// char	*str = "echo hello | grep h > test.txt | cat";
-	char *str = "echo hello ; echo world";
+	// char *str = "echo hello | grep h > text.txt ; cat text.txt ; echo again";
+	char *str = "echo try > to_me again";
+	// char *str = "echo hello ; echo world";
 	t_node	*root;
 
-	root = parse_line(&str);
+	root = parse_line(&str); //make a loop and check for str != NULL and if you encouunter redir in string break out and set it to new root here
 	print_tree(root);
 	write(1, "\n", 1);
 	exec_tree(root);
