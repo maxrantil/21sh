2abe329cf9d4057f23b1047a7d96a02c14154f47
mqx <litnar@pm.me>
2022-11-24 10:04:12 +0200

    redir fixes in tree

--- srcs/parser/parse_redirection.c
+++ srcs/parser/parse_redirection.c
@@ -14,27 +14,34 @@
 
 t_node *parse_redirection(t_node *n, char **ptr_to_line)
 {
-	char *token;
-	char *end_q;
-	int type;
+	t_node	*tmp;
+	t_node	*m;
+	char	*token;
+	char	*end_q;
+	int		type;
 
 	while (peek(ptr_to_line, "<>1234567890"))
 	{
 		type = token_get(ptr_to_line, &token, &end_q);
-		ft_printf("type: %d\n", type);
-		ft_printf("token: %s\n", token);
-		if (ft_strcspn("<>", token) && ft_strchr(token, '&'))
+		if (ft_isdigit(*token) && *(token + 1) == '>' && *(token + 2) == '&' && (ft_isdigit(*(token + 3)) || *(token + 3) == '-')) // this is a hack
 		{
-			n = node_create(FILEAGG, n, NULL);
-			add_to_args(&n->arg, ft_strsub(token, 0, (size_t)(end_q - token)));
-			token_get(ptr_to_line, &token, &end_q);
-			// token_get(ptr_to_line, &token, &end_q);
-			// token_get(ptr_to_line, &token, &end_q);
-			// parse_redirection(n, ptr_to_line);
+			if (n->type == REDIROVER || n->type == REDIRAPP || n->type == REDIRIN)
+			{
+				tmp = n->left;
+				m = node_create(REDIROVER, tmp, NULL);
+				add_to_args(&m->arg, ft_strsub(token, 0, 4)); // 4 here hardcoded
+				*ptr_to_line += 4;	// this is a hack 4 is hardcoded
+				n->left = m;
+				tmp = NULL;
+			}
+			else
+			{
+				n = node_create(FILEAGG, n, NULL);
+				add_to_args(&n->arg, ft_strsub(token, 0, 4)); // 4 here hardcoded
+				*ptr_to_line += 4;	// this is a hack 4 is hardcoded
+			}
 		}
-		// if (peek(ptr_to_line, "1234567890")) //may need '&' here too
-			// n = parse_fileagg(n, ptr_to_line); //else if after?
-		if (token_get(ptr_to_line, &token, &end_q) != 'a' && '&' != *token && !ft_isdigit(*token))
+		else if (token_get(ptr_to_line, &token, &end_q) != 'a' && '&' != *token && !ft_isdigit(*token))
 		{
 			ft_putstr_fd("redir syntax error near unexpected token `", 2); // make better error handling for `echo hello >`
 			ft_putchar_fd(*token, 2);
@@ -42,8 +49,16 @@ t_node *parse_redirection(t_node *n, char **ptr_to_line)
 			tree_free(n);
 			return (NULL);
 		}
-		if (n && (type == '>' || type == '#' || type == '<'))
+		else if (n && (type == '>' || type == '#' || type == '<'))
 		{
+			if (n->type == FILEAGG)
+			{
+				tmp = n->left;
+				m = node_create(REDIROVER, tmp, NULL);
+				add_to_args(&m->arg, ft_strsub(token, 0, (size_t)(end_q - token)));
+				n->left = m;
+				tmp = NULL;
+			}
 			if (type == '>')
 				n = node_create(REDIROVER, n, NULL);
 			else if (type == '<')
