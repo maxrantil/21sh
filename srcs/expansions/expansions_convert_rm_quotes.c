91c0ca55a5e3d543ddb3349342a7ca8de6bc2d8c
Roope Vuorenlehto <rvuorenl@c1r3p6.hive.fi>
2022-12-08 20:20:20 +0200

    clean converions/quotes

--- srcs/expansions/expansions_convert_rm_quotes.c
+++ srcs/expansions/expansions_convert_rm_quotes.c
@@ -6,7 +6,7 @@
 /*   By: rvuorenl <rvuorenl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 18:22:38 by rvuorenl          #+#    #+#             */
-/*   Updated: 2022/12/08 19:38:39 by rvuorenl         ###   ########.fr       */
+/*   Updated: 2022/12/08 20:19:51 by rvuorenl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,60 +57,36 @@ void	getenv_check(char **content, char *env_name)
 	char	*tmp;
 
 	tmp = getenv(env_name);
-	// printf("\t\t(%s)\n", tmp);
 	if (tmp)
 		*content = ft_strdup(tmp);
 	else
 		*content = ft_strdup("");
-	// printf("\t\t(%s)\n", *content);
 }
 
 void	update_second_quote(size_t *len, size_t *q2, size_t cont, size_t var)
 {
-	*len += cont - var - 1;
+	if (len && (*len) != 0)
+		*len += cont - var - 1;
 	if (q2 && (*q2) != 0)
 		*q2 += cont - var - 1;
 }
-
-// void	ft_strclr2(char *s)
-// {
-// 	int	i;
-
-// 	if (s == NULL)
-// 		return ;
-// 	i = 0;
-// 	while (s[i])
-// 	{
-// 		s[i] = '\0';
-// 		i++;
-// 	}
-// }
-
-// void	ft_strdel2(char **as)
-// {
-// 	if (as == NULL)
-// 		return ;
-// 	ft_strclr2(*as);
-// 	free(*as);
-// 	*as = NULL;
-// }
-
 
 size_t	move_counter_content_len(char **content)
 {
 	size_t	cont_len;
 
 	cont_len = ft_strlen(*content);
-	// printf("j\n");
-
 	ft_strdel(content);
-	// ft_strdel2(content);
-
-	// printf("k\n");
 	return (cont_len);
 }
 
-// i pos of $, str = str
+void	fill_env(char **tmp, char **cont, size_t *env_len, char *str)
+{
+	*tmp = get_full_env_name(str);
+	*env_len = ft_strlen(*tmp);
+	getenv_check(cont, *tmp);
+}
+
 size_t	update_arg_dollar(int i, char **str, size_t *len, size_t *q2)
 {
 	char	*tmp;
@@ -119,40 +95,23 @@ size_t	update_arg_dollar(int i, char **str, size_t *len, size_t *q2)
 	size_t	post_len;
 	size_t	env_len;
 
-	// printf("\nDOLLAR\n");
-	// printf("\nDOLLAR\n");
-	// printf("\nDOLLAR\n");
-	// printf("(%s), ", (*str));
-	// printf("len (%zu), ", *len);
-	// printf("q2 (%zu)\n", *q2);
-	printf("a\n");
 	pre_dollar = NULL;
-	tmp = get_full_env_name(&(*str)[i + 1]);
-	printf("b\n");
-	env_len = ft_strlen(tmp);
-	getenv_check(&content, tmp);
-	printf("c\n");
+	fill_env(&tmp, &content, &env_len, &(*str)[i + 1]);
 	update_second_quote(len, q2, ft_strlen(content), ft_strlen(tmp));
 	post_len = ft_strlen(*str) - env_len - (size_t)i - 1;
-	printf("d\n");
 	ft_strdel(&tmp);
 	if (post_len > 0)
 		tmp = get_post_dollar_content(*str, i, env_len, post_len);
-	printf("e\n");
 	if (i > 0)
 		pre_dollar = ft_strsub((const char *)(*str), 0, (size_t)i);
 	ft_strdel(str);
-	printf("f\n");
 	*str = ft_strnew((size_t)i + ft_strlen(content) + post_len);
-	printf("g\n");
 	if (!*str)
 	{
 		ft_putstr_fd("error in strnew update dollar. exit(9)\n", 2);
 		exit(9);
 	}
-	printf("h\n");
 	replace_dollar(str, content, &pre_dollar, &tmp);
-	printf("i\n");
 	return (move_counter_content_len(&content));
 }
 
@@ -161,11 +120,6 @@ static ssize_t	find_matching_quote(char *str, char quote)
 	ssize_t	i;
 
 	i = 1;
-
-	// while (str[i] && str[i] != quote)
-	// 	i++;
-	// return (i);
-
 	while (str[i])
 	{
 		if (str[i] == '\\' && (str[i + 1] == '\'' || str[i + 1] == '\"'))
@@ -174,7 +128,6 @@ static ssize_t	find_matching_quote(char *str, char quote)
 			break ;
 		i++;
 	}
-	// printf("in matching ret i = (%zu)\n", i);
 	return (i);
 }
 
@@ -199,31 +152,17 @@ size_t	strip_quotes_double(char **str, size_t quote1)
 
 	i = quote1;
 	len = ft_strlen(*str);
-	// printf("debug (%s)\n", *str);
 	quote2 = find_matching_quote(&((*str)[quote1]), (*str)[quote1]);
 	quote2 += quote1;
-	// printf("len (%zu), q1 (%zu), q2 (%zu), i (%zu)\n", len, quote1, quote2, i);
 	while (i < quote2)
 	{
-		// printf("inside i (%zu) q2 (%zu)\n", i, quote2);
-		// printf("char (%c)\n", *str[i + 1]);
-		// i++;
 		if ((*str)[i] == '$')
-		{
-			// printf("\tinside IF\n");
 			i += update_arg_dollar(i, str, &len, &quote2);
-		}
 		else
-		{
-			// printf("\tinside ELSE\n");
 			i++;
-		}
 	}
-	// printf("a\n");
 	ft_memmove(&((*str)[quote1]), &((*str)[quote1 + 1]), len - quote1);
-	// printf("b\n");
 	ft_memmove(&((*str)[quote2 - 1]), &((*str)[quote2]), len - quote2);
-	// printf("c\n");
 	return (quote2 - 2);
 }
 
@@ -235,71 +174,21 @@ void	loop_conversions_quotes(t_node *n, t_shell *sh)
 	word = -1;
 	while (n->arg[++word] != NULL)
 	{
+		if (n->arg[word][0] == '~' && (n->arg[word][1] == '/'
+			|| n->arg[word][1] == '\0'))
+			expansions_tilde(n, sh, word);
 		i = -1;
 		while (n->arg[word][++i])
 		{
-			if (n->arg[word][0] == '~' && (n->arg[word][1] == '/'
-				|| n->arg[word][1] == '\0'))
-			{
-				expansions_tilde(n, sh, word);
-			}
 			if (n->arg[word][i] == '\\' && (n->arg[word][i + 1] == '\''
 					|| n->arg[word][i + 1] == '\"'))
 				i++;
 			else if (n->arg[word][i] == '\'')
-			{
-				// starting i = first quote
-				i += strip_quotes_single(n->arg[word], i);
-				// last should be letter before last quote
-			}
+				i = strip_quotes_single(n->arg[word], i);
 			else if (n->arg[word][i] == '\"')
-			{
-				// printf("i (%zu) - ", i);
 				i = strip_quotes_double(&(n->arg[word]), i);
-				// printf("i (%zu)\n", i);
-			}
 			else if (n->arg[word][i] == '$')
-			{
-				size_t	word_len;
-				word_len = ft_strlen(n->arg[word]);
-				i = update_arg_dollar(i, &(n->arg[word]), &word_len, 0);
-				// printf("after dollar (%zu) str(%s)\n", i, n->arg[word]);
-				i--;
-			}
+				i = update_arg_dollar(i, &(n->arg[word]), 0, 0) - 1;
 		}
 	}
 }
-
-/*
-0123456
-'hello'
-
-q1 = 0
-q2 = 6
-len = 7
-
-0123456
-hello
-*/
-
-/*
-
-012345678901
-"$USER.t"
-q2 = 8
-var_len = 4+1
-012345678901	1 -> 9
-"rvuorenl.t"
-q2 = 11
-cont_len = 8
-
-0123456789012
-"$USERNAME.t"
-q2 = 12
-var_len = 8+1
-012345678901	1 -> 3
-"rv.t"
-q2 = 11
-cont_len = 2
-
-*/
