e803600ca8c23c867f1e85e69f20d83af5c9796d
Roope Vuorenlehto <rvuorenl@c1r3p6.hive.fi>
2022-12-12 22:46:18 +0200

    add proper error messages for open_check

--- srcs/exec/input_file_read.c
+++ srcs/exec/input_file_read.c
@@ -1,4 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   input_file_read.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvuorenl <rvuorenl@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/12 22:13:15 by rvuorenl          #+#    #+#             */
+/*   Updated: 2022/12/12 22:45:51 by rvuorenl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_21sh.h"
+
+static void	check_open_errors(char *filename)
+{
+	if (!access(filename, F_OK))
+	{
+		ft_putstr_fd("21sh: ", 2);
+		ft_putstr_fd(filename, 2);
+		ft_putstr_fd(": Permission denied\n", 2);
+	}
+	else
+	{
+		ft_putstr_fd("21sh: ", 2);
+		ft_putstr_fd(filename, 2);
+		ft_putstr_fd(": No such file or directory\n", 2);
+	}
+}
 
 static int	open_read_check(char *filename)
 {
@@ -6,12 +34,7 @@ static int	open_read_check(char *filename)
 
 	file_fd = open(filename, O_RDONLY);
 	if (file_fd == -1)
-	{
-		// check reason ?
-		// ft_putstr_fd(filename, 2);
-		write(2, "open_read fail\n", 15);
-		exit(1);
-	}
+		check_open_errors(filename);
 	return (file_fd);
 }
 
@@ -21,5 +44,6 @@ void	input_file_read(t_node *n, t_shell *sh)
 
 	close(0);
 	file_fd = open_read_check(n->arg[1]);
-	exec_tree(n->left, sh);
+	if (file_fd != -1)
+		exec_tree(n->left, sh);
 }
