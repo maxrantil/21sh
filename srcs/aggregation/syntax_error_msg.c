a2b1a636e6d07841016d710a2f43d6e1820c13ec
Roope Vuorenlehto <rvuorenl@c1r3p6.hive.fi>
2022-12-13 14:01:04 +0200

    fix typo in syntax_error, missing variabletype of argument

--- srcs/aggregation/syntax_error_msg.c
+++ srcs/aggregation/syntax_error_msg.c
@@ -6,14 +6,14 @@
 /*   By: rvuorenl <rvuorenl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 11:44:46 by rvuorenl          #+#    #+#             */
-/*   Updated: 2022/12/13 13:51:49 by rvuorenl         ###   ########.fr       */
+/*   Updated: 2022/12/13 13:59:51 by rvuorenl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_21sh.h"
 
-int	syntax_error_msg(exit_code)
+int	syntax_error_msg(int exit_code)
 {
 	ft_putstr_fd("21sh: Syntax error!\n", 2);
-	return(exit_code);
+	return (exit_code);
 }
