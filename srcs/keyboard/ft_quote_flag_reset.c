3f1bf4b85409d19dac57556b5d1d41a3b49e5d2f
Max <rantil@pm.me>
2022-12-16 16:52:31 +0200

    v3, lets get going

--- srcs/keyboard/ft_quote_flag_reset.c
+++ srcs/keyboard/ft_quote_flag_reset.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_quote_flag_reset.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbarutel <mbarutel@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: mrantil <mrantil@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 09:23:06 by mbarutel          #+#    #+#             */
-/*   Updated: 2022/12/13 17:12:55 by mbarutel         ###   ########.fr       */
+/*   Updated: 2022/12/16 16:51:39 by mrantil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 /**
  * It resets the flags that are used to determine if the user is in a quote
  * or a heredoc
- * 
+ *
  * @param t the structure containing all the information about the current line
  */
 // void	ft_flag_reset(t_term *t)
@@ -47,12 +47,19 @@ void	ft_quote_flag_reset(t_term *t)
 	i = -1;
 	t->q_qty = 0;
 	t->quote = 0;
+	t->heredoc = 0;
 	while (t->inp[++i])
 	{
 		if ((t->inp[i] == D_QUO || t->inp[i] == S_QUO) && !t->heredoc)
 		{
 			if (!ft_bslash_escape_check(t, i))
 				ft_quote_handling(t, t->inp[i]);
+		}
+		if (t->inp[i] == '<')
+		{
+			ft_heredoc_handling(t, i);
+			if (!t->heredoc && t->delim)
+				ft_strdel(&t->delim);
 		}
 	}
 }
